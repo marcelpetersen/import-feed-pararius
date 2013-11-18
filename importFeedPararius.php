4bbfcf4808dc23a89e723241385116f128467715
Joost Hoeks <joosthoeks@gmail.com>
2013-11-18 16:08:50 +0000

    set vars

--- importFeedPararius.php
+++ importFeedPararius.php
@@ -40,6 +40,79 @@ foreach ($parsedXml->member->items->item as $item) {
     echo '<pre>';
     var_dump($item);
     echo '</pre>';
+    // check if isset objectId else continue:
+    if (!isset($item->uniqueobjectid))        continue;
+    // set vars:
+    $uniqueobjectid = $item->uniqueobjectid;
+    $street = (isset($item->street)) ? $item->street : '';
+    $houseNumber = (isset($item->houseNumber)) ? $item->houseNumber : '';
+    $houseNumberAddtion = (isset($item->houseNumberAddtion)) ? $item->houseNumberAddtion : '';
+    $postalCode = (isset($item->postalCode)) ? $item->postalCode : '';
+    $city = (isset($item->city)) ? $item->city : '';
+    $SubArea = (isset($item->SubArea)) ? $item->SubArea : '';
+    $houseType = (isset($item->houseType)) ? $item->houseType : '';
+    $tenant = (isset($item->tenant)) ? $item->tenant : '';
+    $estateOwner = (isset($item->estateOwner)) ? $item->estateOwner : '';
+    $stats = (isset($item->stats)) ? $item->stats : '';
+    $furnished = (isset($item->furnished)) ? $item->furnished : '';
+    $minprice = (isset($item->minprice)) ? $item->minprice : '';
+    $hideprice = (isset($item->hideprice)) ? $item->hideprice : '';
+    $NroOfRooms = (isset($item->NroOfRooms)) ? $item->NroOfRooms : '';
+    $NroOfLivingRooms = (isset($item->NroOfLivingRooms)) ? $item->NroOfLivingRooms : '';
+    $Projectnaam = (isset($item->Projectnaam)) ? $item->Projectnaam : '';
+    $WoningtypeInProject = (isset($item->WoningtypeInProject)) ? $item->WoningtypeInProject : '';
+    $Available = (isset($item->Available)) ? $item->Available : '';
+    $insertDate = (isset($item->insertDate)) ? $item->insertDate : '';
+    $description_nl = (isset($item->description_nl)) ? $item->description_nl : '';
+    $description_fr = (isset($item->description_fr)) ? $item->description_fr : '';
+    $description_en = (isset($item->description_en)) ? $item->description_en : '';
+    $description_de = (isset($item->description_de)) ? $item->description_de : '';
+    $description_es = (isset($item->description_es)) ? $item->description_es : '';
+    $description_it = (isset($item->description_it)) ? $item->description_it : '';
+    $size_m2 = (isset($item->size_m2)) ? $item->size_m2 : '';
+    $numberOfBathrooms = (isset($item->numberOfBathrooms)) ? $item->numberOfBathrooms : '';
+    $contractLentgh_months = (isset($item->contractLentgh_months)) ? $item->contractLentgh_months : '';
+    $minContractLentgh_months = (isset($item->minContractLentgh_months)) ? $item->minContractLentgh_months : '';
+    $buildYear = (isset($item->buildYear)) ? $item->buildYear : '';
+    $Parking = (isset($item->Parking)) ? $item->Parking : '';
+    $bath = (isset($item->bath)) ? $item->bath : '';
+    $separateShower = (isset($item->separateShower)) ? $item->separateShower : '';
+    $separateToilet = (isset($item->separateToilet)) ? $item->separateToilet : '';
+    $lift = (isset($item->lift)) ? $item->lift : '';
+    $garden = (isset($item->garden)) ? $item->garden : '';
+    $gardenLigging = (isset($item->gardenLigging)) ? $item->gardenLigging : '';
+    $gardenSizeM2 = (isset($item->gardenSizeM2)) ? $item->gardenSizeM2 : '';
+    $roofTerrass = (isset($item->roofTerrass)) ? $item->roofTerrass : '';
+    $roofTerrassLigging = (isset($item->roofTerrassLigging)) ? $item->roofTerrassLigging : '';
+    $roofTerrassSizeM2 = (isset($item->roofTerrassSizeM2)) ? $item->roofTerrassSizeM2 : '';
+    $balcony = (isset($item->balcony)) ? $item->balcony : '';
+    $BalconyLigging = (isset($item->BalconyLigging)) ? $item->BalconyLigging : '';
+    $BalconySizeM2 = (isset($item->BalconySizeM2)) ? $item->BalconySizeM2 : '';
+    $swimmingPool = (isset($item->swimmingPool)) ? $item->swimmingPool : '';
+    $airConditioning = (isset($item->airConditioning)) ? $item->airConditioning : '';
+    $firePlace = (isset($item->firePlace)) ? $item->firePlace : '';
+    $garage = (isset($item->garage)) ? $item->garage : '';
+    $cellar = (isset($item->cellar)) ? $item->cellar : '';
+    $publicTransportQualityID = (isset($item->publicTransportQualityID)) ? $item->publicTransportQualityID : '';
+    $showhouseNumber = (isset($item->showhouseNumber)) ? $item->showhouseNumber : '';
+    $groundFloor = (isset($item->groundFloor)) ? $item->groundFloor : '';
+    $floorQuality = (isset($item->floorQuality)) ? $item->floorQuality : '';
+    $rentIncluded = (isset($item->rentIncluded)) ? $item->rentIncluded : '';
+    $brochure = (isset($item->brochure)) ? $item->brochure : FALSE;
+    if ($brochure) {
+        // get brochure:
+        
+    }
+    $plattegrond = (isset($item->plattegrond)) ? $item->plattegrond : FALSE;
+    if ($plattegrond) {
+        // get plattegrond:
+        
+    }
+    $updatePhotos = (isset($item->updatePhotos)) ? $item->updatePhotos : 0;
+    if (!isset($xml->photos))        continue;
+    if (!isset($xml->photos->photo))        continue;
+    // get photo:
+    
 }
 
 // functions:
